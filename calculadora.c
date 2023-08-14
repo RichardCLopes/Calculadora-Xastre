5cc7c8b6208d427c3301c8c2a68a4af8216c0b2c
Richard do Carmo Lopes <32149536+RichardCLopes@users.noreply.github.com>
2023-08-14 09:05:18 -0300

    Update

--- calculadora.c
+++ calculadora.c
@@ -2,8 +2,11 @@
 #include<stdlib.h>
 
 int main(){
-    int sair=0;
+    int sair=0, opcao;
+    int valor_a, valor_b;
     do {
-        
+        printf("Operacao\n1-Soma\n2-Subtracao\n3-Divisao\n4-Multiplicacao\n5-Sair:\n");
+        scanf("%d", &opcao);
+        printf("%d", opcao);
     }while(sair==0);
 }
