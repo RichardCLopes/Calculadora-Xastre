e8bf7860bb3c18762f240573a863a77be4efee68
Gabriel <gabsazeredo@gmail.com>
2023-08-16 08:54:33 -0300

    Update

--- calculadora.c
+++ calculadora.c
@@ -7,7 +7,6 @@ int main(){
     do {
         printf("\nOperacao\n1-Soma\n2-Subtracao\n3-Divisao\n4-Multiplicacao\n5-Sair:");
         scanf("%d", &opcao);
-        printf("%d", opcao);
         printf("Numero 1 :");
         scanf("%f", &valor_a);
         printf("Numero 2 :");
